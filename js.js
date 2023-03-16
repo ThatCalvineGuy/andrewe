7878fa6b87d1d3375d48cab69bc0eec36d6b80b4
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-03-16 14:45:09 -0700

    removed javascript

--- js.js
+++
@@ -1,8 +0,0 @@
-<script>
-var images = ["image1.jpg", "image2.jpg"];
-var current = 0;
-function swapImage() {
-  current = (current + 1) % images.length;
-  document.body.style.backgroundImage = "url('" + images[current] + "')";
-}
-</script>
