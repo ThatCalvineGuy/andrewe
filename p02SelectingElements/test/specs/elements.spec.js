bf2d56b74502e279bceab2edd62530e167bde3eb
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-13 14:39:35 -0700

    placed spec in a for loop

--- p02SelectingElements/test/specs/elements.spec.js
+++ p02SelectingElements/test/specs/elements.spec.js
@@ -1,25 +1,22 @@
 import ElementsPage from "../pageobjects/add_remove_elements.page.js";
 
 describe('Elements page', () => {
-    it('should create 5 elements and delete 3 leaving 2 remaining', async () => {
+    it('should create 5 elements, delete 2, and leave 3 remaining', async () => {
         await ElementsPage.open()
         await browser.pause(3000)
-        await (ElementsPage).clickAddButton()
+
+        await for (; '#elements' < 5; ;) {
+            (ElementsPage).AddButton.click();
+            browser.pause(1000);
+        }
+        await expect('#elements').toHaveChildren(5)
         await browser.pause(3000)
-        await (ElementsPage).clickAddButton()
+
+        await for (; '#elements' > 3; ;) {
+            (ElementsPage).clickDeleteButton.click();
+            browser.pause(1000);
+        }
+        await expect('#elements').toHaveChildren(3)
         await browser.pause(3000)
-        await (ElementsPage).clickAddButton()
-        await browser.pause(3000)
-        await (ElementsPage).clickAddButton()
-        await browser.pause(3000)
-        await (ElementsPage).clickAddButton()
-        await browser.pause(3000)
-        await (ElementsPage).clickDeleteButton()
-        await browser.pause(3000)
-        await (ElementsPage).clickDeleteButton()
-        await browser.pause(3000)
-        await (ElementsPage).clickDeleteButton()
-        await browser.pause(3000)
-        await expect('#elements').toHaveChildren(2)
     });
 });
