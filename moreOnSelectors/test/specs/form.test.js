a54ccbad8c249e3fff1df1a4ec6d68bd4ca9230e
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-18 15:32:20 -0700

    attempted to utilize new page objects

--- moreOnSelectors/test/specs/form.test.js
+++ moreOnSelectors/test/specs/form.test.js
@@ -5,9 +5,45 @@ describe('My Form application', () => {
         await FormPage.open();
         await expect(FormPage.header).toHaveTextContaining('Form validation');
     })
-    it('should select an option from a dropdown', async () => {
+    it('should provide 9 errors when submitting without entering anything in', async () =>{
+        await FormPage.submit.click();
+        await expect(FormPage.errortext).toHaveTextContaining('The form could not be submitted because 9 errors were found.')
+    })
+    it('should select a title', async () => {
         await FormPage.title.click();
         await FormPage.titleesq.click();
         await expect(FormPage.title).toHaveTextContaining('Esq.')
     })
+    it('should enter a name', async () => {
+        await FormPage.inputFirstName.setValue('Andrew');
+        await FormPage.inputLastName.setValue('Elie')
+    })
+    it('should enter an email', async () => {
+        await FormPage.inputEmail.setValue('andrewe@autest.net');
+    })
+    it('should enter a phone number', async () => {
+        await FormPage.inputPhoneNumber.setValue('3131234567');
+    })
+    it('should enter a postal code', async () => {
+        await FormPage.inputEmail.setValue('X7M1M7');
+    })
+    xit('should upload a file', async () => {
+        //browser.uploadFile(localpath)
+    })
+    it('should agree to the terms and conditions', async () => {
+        await FormPage.checkAgree.click();
+    })
+    it('should select fish as favorite pet', async () => {
+        await FormPage.selectPetFish.click();
+    })
+    it('should claim canadian citizenship', async () => {
+        await FormPage.selectCitizen.click();  
+    })
+
+
+    it('should only throw up one error when submitting', async () => {
+        await FormPage.submit.click();
+        await expect(FormPage.errortext).toHaveTextContaining('The form could not be submitted because 1 error was found.')
+    })
+    
 });
