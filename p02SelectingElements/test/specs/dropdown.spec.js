270ce7b3a842326621f6a75d1dcff41b06be1071
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-18 14:13:35 -0700

    edited lesson 8 pages for style points

--- p02SelectingElements/test/specs/dropdown.spec.js
+++ p02SelectingElements/test/specs/dropdown.spec.js
@@ -3,17 +3,13 @@ import dropdownPage from "../pageobjects/dropdown.page.js";
 describe('Dropdown page', () => {
     it('should select option 1', async () => {
         await dropdownPage.open()
-        await browser.pause(3000)
         await (dropdownPage).dDown.click()
-        await browser.pause(3000)
         await (dropdownPage).option1.click()
         await expect(dropdownPage.option1).toBeSelected()
     });
     it('should select option 2', async () => {
         await dropdownPage.open()
-        await browser.pause(3000)
         await (dropdownPage).dDown.click()
-        await browser.pause(3000)
         await (dropdownPage).option2.click()
         await expect(dropdownPage.option2).toBeSelected()
     });
--- p02SelectingElements/test/specs/elements.spec.js
+++ p02SelectingElements/test/specs/elements.spec.js
@@ -1,21 +1,18 @@
 import ElementsPage from "../pageobjects/add_remove_elements.page.js";
 
 describe('Elements page', () => {
-    it('should create 5 elements, delete 2, and leave 3 remaining', async () => {
+    it('should create 5 elements', async () => {
         await ElementsPage.open()
-        await browser.pause(3000)
 
         for (let i=(ElementsPage).buttonCount; i < 5; i++) {
             await (ElementsPage).addButton.click();
-            await browser.pause(1500);
         }
-        await browser.pause(3000)
-
+        await expect (ElementsPage).buttoncount == 5
+    });
+    it ('should delete 2 elements, leaving 3 remaining', async () => {
         for (let i=(ElementsPage).buttonCount; i > 3; i--) {
             await (ElementsPage).deleteButton.click();
-            await browser.pause(1500);
         }
-        await browser.pause(3000)
         await expect (ElementsPage).buttoncount == 3
     });
 });
