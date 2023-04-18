bd632d86b7c8dbb45eff21582fde8cb7fe4f5cb7
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-18 15:31:25 -0700

    added lesson 9 page objects

--- moreOnSelectors/test/pageobjects/form.page.js
+++ moreOnSelectors/test/pageobjects/form.page.js
@@ -2,19 +2,40 @@ import Page from '../pageobjects/page.js'
 class FormPage extends Page {
      //define multiple selectors using getter methods
     get header() {
-        return $('//*[@id="wb-cont"]'); //uses Element with certain text selector
+        return $('#wb-cont'); //uses Element with certain text selector
+    }
+    get inputFirstName() {
+        return $('#fname1');
+    }
+    get inputLastName() {
+        return $('#lname1');
+    }
+    get inputPhoneNumber() {
+        return $('#tel1');
+    }
+    get inputPostalCode() {
+        return $('#pcodeca1');
     }
      // Email Selector
-     get inputEmail() {
-          return $('//*[@id="email1"]'); //uses CSS query selector
-     }
+    get inputEmail() {
+        return $('#email1'); //uses CSS query selector
+    }
+    get checkAgree() {
+        return $('#agree1');
+    }
+    get selectPetFish() {
+        return $('#animal3');
+    }
+    get selectCitizen() {
+        return $('#status1');
+    } 
     get submit()
     {
-        return $('//*[@id="validation-example"]/input[1]');
+        return $('#validation-example > input.btn.btn-primary');
     }
     get errortext()
     {
-        return $('#errors-validation-example > h2:nth-child(1)');
+        return $('#errors-validation-example > h2');
     }
     get title() {
         return $('#title1')
@@ -22,13 +43,8 @@ class FormPage extends Page {
     get titleesq() {
         return $('#title1 > option:nth-child(3)')
     }
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to enter in a valid email
-     */
-    async emailInput() {
-        await this.emailInput.setValue(input);
-        await this.submit.click();
+    get titlemr() {
+        return $('#title1 > option:nth-child(4)')
     }
      /**
       * overwrite specific options to adapt it to page object
