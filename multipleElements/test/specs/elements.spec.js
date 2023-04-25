d9a53de49b59b96ff5f89dd0e9f8c3159033043a
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-25 15:54:18 -0700

    consolidated add/remove element code

--- multipleElements/test/specs/elements.spec.js
+++ multipleElements/test/specs/elements.spec.js
@@ -1,13 +1,17 @@
 import ElementsPage from "../pageobjects/add_remove_elements.page.js";
 
 describe('Elements page', () => {
-    it('should create 4 elements', async () => {
+    it('should have 5 elements', async () => {
         await ElementsPage.open()
-        for (let i=(ElementsPage).buttonCount; i < 4; i++) {
-            await (ElementsPage).addButton.click();
-        }
+        await ElementsPage.addButtons(5);
+        await expect (ElementsPage).buttoncount == 5
     });
-    it('should have 4 elements', async () => {
-        await expect (ElementsPage).buttoncount == 4
+    it('should have 3 elements', async () => {
+        await ElementsPage.deleteButtons(3);
+        await expect (ElementsPage).buttoncount == 3
+    });
+    it('should have 10 elements', async () => {
+        await ElementsPage.addButtons(10);
+        await expect (ElementsPage).buttoncount == 10
     });
 });
