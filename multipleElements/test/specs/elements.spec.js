fe738b9420b4a4cd72b17a41e168a9a0c1f2c7fc
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-25 13:48:28 -0700

    lesson 11 complete

--- multipleElements/test/specs/elements.spec.js
+++ multipleElements/test/specs/elements.spec.js
@@ -1,18 +1,13 @@
 import ElementsPage from "../pageobjects/add_remove_elements.page.js";
 
 describe('Elements page', () => {
-    it('should create 5 elements', async () => {
+    it('should create 4 elements', async () => {
         await ElementsPage.open()
-
-        for (let i=(ElementsPage).buttonCount; i < 5; i++) {
+        for (let i=(ElementsPage).buttonCount; i < 4; i++) {
             await (ElementsPage).addButton.click();
         }
-        await expect (ElementsPage).buttoncount == 5
     });
-    it ('should delete 2 elements, leaving 3 remaining', async () => {
-        for (let i=(ElementsPage).buttonCount; i > 3; i--) {
-            await (ElementsPage).deleteButton.click();
-        }
-        await expect (ElementsPage).buttoncount == 3
+    it('should have 4 elements', async () => {
+        await expect (ElementsPage).buttoncount == 4
     });
 });
