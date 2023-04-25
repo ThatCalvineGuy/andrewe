dae504da8b2a391cbed7f36afa236f848210b583
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-25 14:51:47 -0700

    lesson 11 done (removed redundant open commands)

--- multipleElements/test/specs/home.spec.js
+++ multipleElements/test/specs/home.spec.js
@@ -5,11 +5,9 @@ describe('Home Page', () => {
         await expect(homePage.listExamples).toBeElementsArrayOfSize(44);
     });
     it('should not have 45 list items or more', async () => {
-        await homePage.open();
         await expect(homePage.listExamples).not.toBeElementsArrayOfSize({gte: 45});
     });
     it('should not have 43 list items or fewer', async () => {
-        await homePage.open();
         await expect(homePage.listExamples).not.toBeElementsArrayOfSize({lte: 43});
     });
 ;
