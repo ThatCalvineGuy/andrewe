ab451614fb24d06ce2cb307a5d1b63d0fa2c8810
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-25 15:54:41 -0700

    consolidated add/remove element code

--- multipleElements/test/pageobjects/add_remove_elements.page.js
+++ multipleElements/test/pageobjects/add_remove_elements.page.js
@@ -1,16 +1,24 @@
 import Page from './page.js';
 class ElementsPage extends Page {
 
-  get addButton() { 
+  get addaButton() { 
     return $('button[onclick="addElement()"]') 
   }
-  get deleteButton() { 
+  get deleteaButton() { 
     return $('button[onclick="deleteElement()"]') 
   }  
   get buttonCount() {
     return $$('#added-manually').length;
   }
-  open () {
+  async addButtons(number) {
+    for (let i=(ElementsPage).buttonCount; i < number; i++) {
+      await (ElementsPage).addaButton.click();
+  }}
+  async deleteButtons(number) {
+    for (let i=(ElementsPage).buttonCount; i > number; i--) {
+      await (ElementsPage).deleteaButton.click();
+  }}
+  open() {
     return super.open('add_remove_elements');
 }}
 export default new ElementsPage();
