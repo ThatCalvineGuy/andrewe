04d67eae2c4fe0660c269eeccccf2c77631d5005
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-25 14:38:01 -0700

    lesson 11 complete w/ bonus objectives

--- multipleElements/test/specs/home.spec.js
+++ multipleElements/test/specs/home.spec.js
@@ -1,7 +1,16 @@
 import homePage from "../pageobjects/home.page.js";
 describe('Home Page', () => {
-    it('should verify list items', async () => {
+    it('should have 44 list items', async () => {
         await homePage.open();
-        await expect(homePage.listExamples).toBeElementsArrayOfSize({gte: 1});
+        await expect(homePage.listExamples).toBeElementsArrayOfSize(44);
     });
+    it('should not have 45 list items', async () => {
+        await homePage.open();
+        await expect(homePage.listExamples).not.toBeElementsArrayOfSize(45);
+    });
+    it('should not have 43 list items', async () => {
+        await homePage.open();
+        await expect(homePage.listExamples).not.toBeElementsArrayOfSize(43);
+    });
+;
 });
