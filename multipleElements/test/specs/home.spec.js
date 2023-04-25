64c79f64114cf3e4abd4ff9a62b51df158efda2d
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-25 13:43:55 -0700

    replaced hard coded number with gte

--- multipleElements/test/specs/home.spec.js
+++ multipleElements/test/specs/home.spec.js
@@ -2,6 +2,6 @@ import homePage from "../pageobjects/home.page.js";
 describe('Home Page', () => {
     it('should verify list items', async () => {
         await homePage.open();
-        await expect(homePage.listExamples).toBeElementsArrayOfSize(44);
+        await expect(homePage.listExamples).toBeElementsArrayOfSize({gte: 1});
     });
 });
