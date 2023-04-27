2d4b733d3b93dce62794e34b4b1aa1120b3291e8
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-04-27 14:21:18 -0700

    lesson 12 (recap)

---
+++ recapWdio/test/specs/load.spec.js
@@ -0,0 +1,17 @@
+import dynamic_loadingPage from "../pageobjects/dynamic_loading.page.js";
+describe('My Dynamic Loading Page', () => {
+    it('should verify that start button exists', async () => {
+        await dynamic_loadingPage.open();
+        await dynamic_loadingPage.startButton.waitForClickable();
+        await expect(dynamic_loadingPage.startButton).toBeDisplayed();
+    });
+    it('should show loading bar after clicking button', async () => {
+        await dynamic_loadingPage.startButton.click();
+        await dynamic_loadingPage.loadingBar.waitForDisplayed();
+        await expect(dynamic_loadingPage.loadingBar).toBeDisplayed();
+    })
+    it('should show Hello World text after loading bar', async () => {
+        await dynamic_loadingPage.finishText.waitForDisplayed();
+        await expect(dynamic_loadingPage.finishText).toBeDisplayed();
+    })
+})
