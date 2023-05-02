30ce8dcaf3791261550ca4ce7ed063ee328c1e81
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-05-02 14:04:34 -0700

    TA200 Start lesson 1

---
+++ savingScreenshots/test/pageobjects/dynamic_loading.page.js
@@ -0,0 +1,17 @@
+import Page from './page.js';
+
+class dynamic_loadingPage extends Page {
+
+get startButton() {
+    return $('#start > button');
+}
+get finishText() {
+    return $('#finish > h4');
+}
+get loadingBar() {
+    return $('#loading > img');
+}
+open () {
+    return super.open('dynamic_loading/2');
+}}
+export default new dynamic_loadingPage();
---
+++ savingScreenshots/test/specs/load.spec.js
@@ -0,0 +1,19 @@
+import dynamic_loadingPage from "../pageobjects/dynamic_loading.page.js";
+describe('My Dynamic Loading Page', () => {
+    it('should verify that start button exists', async () => {
+        await dynamic_loadingPage.open();
+        await dynamic_loadingPage.startButton.waitForClickable();
+        await expect(dynamic_loadingPage.startButton).toBeDisplayed();
+        await browser.saveScreenshot('savingScreenshots', 'startScreen.png');
+    });
+    it('should show loading bar after clicking button', async () => {
+        await dynamic_loadingPage.startButton.click();
+        await dynamic_loadingPage.loadingBar.waitForDisplayed();
+        await expect(dynamic_loadingPage.loadingBar).toBeDisplayed();
+    })
+    it('should show Hello World text after loading bar', async () => {
+        await dynamic_loadingPage.finishText.waitForDisplayed();
+        await expect(dynamic_loadingPage.finishText).toBeDisplayed();
+        await browser.saveScreenshot('savingScreenshots','finishScreen.png')
+    })
+})
