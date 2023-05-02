915bede58cc7d3cd87dcb654bf7772feb7d5824c
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-05-02 14:16:00 -0700

    modified  screenshot pathnames

--- savingScreenshots/test/specs/load.spec.js
+++ savingScreenshots/test/specs/load.spec.js
@@ -4,7 +4,7 @@ describe('My Dynamic Loading Page', () => {
         await dynamic_loadingPage.open();
         await dynamic_loadingPage.startButton.waitForClickable();
         await expect(dynamic_loadingPage.startButton).toBeDisplayed();
-        await browser.saveScreenshot('savingScreenshots', 'startScreen.png');
+        await browser.saveScreenshot('..','savingScreenshots', 'startScreen.png');
     });
     it('should show loading bar after clicking button', async () => {
         await dynamic_loadingPage.startButton.click();
@@ -14,6 +14,6 @@ describe('My Dynamic Loading Page', () => {
     it('should show Hello World text after loading bar', async () => {
         await dynamic_loadingPage.finishText.waitForDisplayed();
         await expect(dynamic_loadingPage.finishText).toBeDisplayed();
-        await browser.saveScreenshot('savingScreenshots','finishScreen.png')
+        await browser.saveScreenshot('..','savingScreenshots','finishScreen.png')
     })
 })
