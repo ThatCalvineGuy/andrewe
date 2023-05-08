a64d2860e90b7c892592a85bd850feb249a431aa
Andrew Elie <andrew.l.elie@outlook.com>
2023-05-07 19:15:06 -0700

    why did i call this function twice? smh

--- allTests/test/specs/java_alert.spec.js
+++ allTests/test/specs/java_alert.spec.js
@@ -3,15 +3,14 @@ import JavaScriptAlertsPage from '../pageobjects/javascript_alerts.page.js';
 describe('JavaScript Alerts', () => {
     it('displays an alert when the "Click for JS Alert" button is clicked', async () => {
         await JavaScriptAlertsPage.open();
-        await JavaScriptAlertsPage.checkResult('alert')
-        expect(await JavaScriptAlertsPage.checkResult()).toBeTruthy();
+        expect(await JavaScriptAlertsPage.checkResult('alert')).toBeTruthy();
     });
     it('displays a confirmation when the "Click for JS Confirm" button is clicked', async () => {
-        await JavaScriptAlertsPage.checkResult('confirm')
-        expect(await JavaScriptAlertsPage.checkResult()).toBeTruthy();
+        await JavaScriptAlertsPage.open();
+        expect(await JavaScriptAlertsPage.checkResult('confirm')).toBeTruthy();
     });
     it('displays a prompt when the "Click for JS Prompt" button is clicked', async () => {
-        await JavaScriptAlertsPage.checkResult('Hello World!')
-        expect(await JavaScriptAlertsPage.checkResult()).toBeTruthy();
+        await JavaScriptAlertsPage.open();
+        expect(await JavaScriptAlertsPage.checkResult('Hello World!')).toBeTruthy();
     });
 });
