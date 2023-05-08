1b5f158c36df98b8609bede4929774f91a88b4ce
ThatCalvineGuy <andrew.l.elie@outlook.com>
2023-05-08 14:23:11 -0700

    removed max instance limit

--- allTests/wdio.conf.js
+++ allTests/wdio.conf.js
@@ -45,7 +45,6 @@ export const config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -53,12 +52,10 @@ export const config = {
     //
     capabilities: [
         {
-            maxInstances: 5,
             browserName: 'chrome',
             acceptInsecureCerts: true
         },
         {
-            maxInstances: 5,
             browserName: 'firefox',
             acceptInsecureCerts: true
         }
