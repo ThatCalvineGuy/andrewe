1bebf16f2178b569dc4ae60ae693aef1d5ef7674
Andrew Elie <andrew.l.elie@outlook.com>
2023-05-09 18:47:21 -0700

    prevented false negative

--- allTests/test/specs/home.spec.js
+++ allTests/test/specs/home.spec.js
@@ -5,7 +5,7 @@ describe('Home Page', () => {
         await expect(homePage.listExamples).toBeElementsArrayOfSize(44);
     });
     it('should not have 45 list items or more', async () => {
-        await expect(homePage.listExamples).not.toBeElementsArrayOfSize(45);
+        await expect(homePage.listExamples).not.toBeElementsArrayOfSize({gte:45});
     });
     it('should not have 43 list items or fewer', async () => {
         await expect(homePage.listExamples).not.toBeElementsArrayOfSize({lte: 43});
