e9407d54c9ab2f32c30e804c36065920c0323868
Andrew Elie <andrew.l.elie@outlook.com>
2023-05-09 18:51:44 -0700

    corrected syntax for expect

--- allTests/test/specs/elements.spec.js
+++ allTests/test/specs/elements.spec.js
@@ -4,15 +4,15 @@ describe('Elements page', () => {
     it('should have 5 elements', async () => {
         await ElementsPage.open()
         await ElementsPage.setButtons(5);
-        await expect (ElementsPage).buttoncount == 5
+        await expect(ElementsPage.buttoncount).toBe(5);
     });
     it('should have 3 elements', async () => {
         await ElementsPage.setButtons(3);
-        await expect (ElementsPage).buttoncount == 3
+        await expect(ElementsPage.buttoncount).toBe(3);
     });
     it('should have 10 elements', async () => {
         await ElementsPage.setButtons(10);
-        await expect (ElementsPage).buttoncount == 10
+        await expect(ElementsPage.buttoncount).toBe(10);
     });
     it('should throw an error for negative numbers', async () => {
         try {
