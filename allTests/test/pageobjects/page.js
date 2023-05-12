7ce97e05d28bfb2de8a1cc5325844c3ac8a766d8
Andrew Elie <andrew.l.elie@outlook.com>
2023-05-11 22:33:30 -0700

    placed generateGetFunction outside of Page class

--- allTests/test/pageobjects/page.js
+++ allTests/test/pageobjects/page.js
@@ -1,18 +1,18 @@
 export default class Page {
   open(path) {
     return browser.url(`https://the-internet.herokuapp.com/${path}`);
-    }
-    generateGetFunction(selector, name, isMultiple = false) {
-      return {
-        get [name]() {
-          const element = isMultiple ? $$(selector) : $(selector);
-          expect(element).not.toBe(null);
-          if (isMultiple) {
-            expect(element.length).toBeGreaterThan(0);
-          }
-          return element;
-        },
-      };
-    }
   }
-  
+}
+
+export function generateGetFunction(selector, name, isMultiple = false) {
+  return {
+    get [name]() {
+      const element = isMultiple ? $$(selector) : $(selector);
+      expect(element).not.toBe(null);
+      if (isMultiple) {
+        expect(element.length).toBeGreaterThan(0);
+      }
+      return element;
+    },
+  };
+}
