b8c59b57e5f5a6b757d0d45b1ec3f1e2c7f19b94
Andrew Elie <andrew.l.elie@outlook.com>
2023-05-11 22:13:37 -0700

    made get functions easier to error check

--- allTests/test/pageobjects/page.js
+++ allTests/test/pageobjects/page.js
@@ -4,10 +4,23 @@
 */
 export default class Page {
     /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
-    */
+     * Opens a sub page of the page
+     * @param path path of the sub page (e.g. /path/to/page.html)
+     */
     open (path) {
         return browser.url(`https://the-internet.herokuapp.com/${path}`)
     }
-}
+    generateGetFunction(selector, name, isMultiple = false) {
+      return {
+        get [name]() {
+          const element = isMultiple ? $$(selector) : $(selector);
+          expect(element).not.toBe(null);
+          if (isMultiple) {
+            expect(element.length).toBeGreaterThan(0);
+          }
+          return element;
+        },
+      };
+    }
+  }
+  
